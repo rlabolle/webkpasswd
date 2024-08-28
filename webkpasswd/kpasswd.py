328194190f8ae55a7aac172016bcbf3a99f11a06
Romain Labolle <romain.labolle@universite-lyon.fr>
2024-08-28 16:56:11 +0200

    Minor bugfix and translation update

--- webkpasswd/kpasswd.py
+++ webkpasswd/kpasswd.py
@@ -7,6 +7,8 @@ from .errors import ErrorMsg, ChangePasswordError
 changePasswordErrorMsg = ErrorMsg({
   -1765328366: _("Your account is locked out"),
   -1765328360: _("Old password incorrect"),
+  -1765328378: _("Unknown username"),
+  -1765328228: _("Service unavailable"),
             4: _("The new password was rejected by the server"),
 })
 
@@ -19,7 +21,7 @@ def chgpasswd(username, oldpassword, newpassword):
         errmsg = changePasswordErrorMsg[e.args[1]]
         if e.args[1] == 4:
             try:
-                seen = pwnedpasswords.check(form.newpassword.data, plain_text=True)
+                seen = pwnedpasswords.check(newpassword, plain_text=True)
                 if seen > 0:
                     errmsg = _("This password has been seen %(value)d times before in data breaches", value=seen)
             except:
