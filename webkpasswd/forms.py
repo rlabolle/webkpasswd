61e99c681a365be972a3fd53b0f7e8ca69727810
Romain Labolle <romain.labolle@universite-lyon.fr>
2026-02-26 16:53:51 +0100

    Input field validators: use InputRequired everywhere instead of DataRequired

--- webkpasswd/forms.py
+++ webkpasswd/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, InputRequired, EqualTo
+from wtforms.validators import InputRequired, EqualTo
 from flask_babel import lazy_gettext as _
 
 class ChangePasswordForm(FlaskForm):
-    username    = StringField(_('Username'), validators=[DataRequired()])
-    oldpassword = PasswordField(_('Old password'), validators=[DataRequired()])
+    username    = StringField(_('Username'), validators=[InputRequired()])
+    oldpassword = PasswordField(_('Old password'), validators=[InputRequired()])
     newpassword = PasswordField(_('New password'), validators=[InputRequired()])
     conpassword = PasswordField(_('Confirm new password'), validators=[InputRequired(), EqualTo('newpassword', message=_('Passwords must match'))])
     recaptcha   = RecaptchaField()
